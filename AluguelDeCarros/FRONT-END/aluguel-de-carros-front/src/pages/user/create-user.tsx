7e99abf9e7241a196fc778c8fd792a36ea84d46f
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2023-12-12 22:02:30 -0300

    estilizada bem basicona e zuada na pagina (enfeitarei mais essa pagina no futuro)

--- AluguelDeCarros/FRONT-END/aluguel-de-carros-front/src/pages/user/create-user.tsx
+++ AluguelDeCarros/FRONT-END/aluguel-de-carros-front/src/pages/user/create-user.tsx
@@ -1,19 +1,22 @@
 import Header from './../../app/components/Header/Header';
 
-
+import './create-user.css';
 
 
 
 export default function CreateUser(){
 
     return(
-        <form method="post">
+        <div className="formzao">
+            <form method="post" className="form-itself">
 
-            <input type="text" name="email" placeholder="Email"/><br /><br />
-            <input type="text" name="nome" placeholder="Nome"/><br /><br />
-            <input type="password" name="senha " placeholder="Senha" />
-                        
-        </form>
+                <input type="text" name="email" placeholder="Email"/><br /><br />
+                <input type="text" name="nome" placeholder="Nome"/><br /><br />
+                <input type="password" name="senha " placeholder="Senha" />
+
+            </form>
+            <div className="ocupando-espaço"></div>
+        </div>
     )
 
 
